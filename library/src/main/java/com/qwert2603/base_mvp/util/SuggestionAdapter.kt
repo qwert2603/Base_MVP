ad62dbe3d5d02b27630ce7d9cb06f629e8512cbc
alex <qwert2603@mail.ru>
2017-05-03 10:15:24 +0700

    \

--- library/src/main/java/com/qwert2603/base_mvp/util/SuggestionAdapter.kt
+++ library/src/main/java/com/qwert2603/base_mvp/util/SuggestionAdapter.kt
@@ -12,13 +12,16 @@ import android.widget.ArrayAdapter
 import com.qwert2603.base_mvp.R
 import kotlinx.android.synthetic.main.item_suggestion.view.*
 
-class SuggestionAdapter(context: Context, val search: String, suggestions: List<String>) : ArrayAdapter<String>(context, 0, suggestions) {
+class SuggestionAdapter(context: Context, s: String, suggestions: List<String>) : ArrayAdapter<String>(context, 0, suggestions) {
+
+    val search = s.toLowerCase()
+
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view: View = convertView ?: parent.inflate(R.layout.item_suggestion)
         val s = getItem(position)
         val spannableStringBuilder = SpannableStringBuilder(s)
-        val indexOf = s.indexOf(search)
+        val indexOf = s.toLowerCase().indexOf(search)
         if (indexOf >= 0) {
             spannableStringBuilder.setSpan(StyleSpan(Typeface.BOLD), indexOf, indexOf + search.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
         }
