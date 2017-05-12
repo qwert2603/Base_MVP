0bb3cd471fae489cf68f478b59f387d22afde1fb
alex <qwert2603@mail.ru>
2017-05-12 20:57:47 +0700

    \

--- library/src/main/java/com/qwert2603/base_mvp/BaseApplication.kt
+++ library/src/main/java/com/qwert2603/base_mvp/BaseApplication.kt
@@ -1,11 +1,11 @@
 package com.qwert2603.base_mvp
 
 import android.app.Application
-import com.qwert2603.base_mvp.di.BaseDIManager
 import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache
 import com.nostra13.universalimageloader.core.DisplayImageOptions
 import com.nostra13.universalimageloader.core.ImageLoader
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
+import com.qwert2603.base_mvp.di.BaseDIManager
 
 abstract class BaseApplication : Application() {
 
@@ -24,7 +24,7 @@ abstract class BaseApplication : Application() {
                 .showImageOnLoading(R.drawable.ic_image_white)
                 .build()
         val config = ImageLoaderConfiguration.Builder(this)
-                .memoryCacheSize(12 * 1024 * 1024)
+                .memoryCacheSize(imagesMemoryCacheMB() * 1024 * 1024)
                 .diskCache(UnlimitedDiskCache(cacheDir))
                 .defaultDisplayImageOptions(displayImageOptions)
                 .build()
@@ -32,4 +32,6 @@ abstract class BaseApplication : Application() {
     }
 
     abstract fun createDIManager(): BaseDIManager
+
+    open protected fun imagesMemoryCacheMB() = 48
 }
