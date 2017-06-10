c9c011d2b680ccfe81de0a56bb4d012aad184cc8
alex <qwert2603@mail.ru>
2017-06-10 15:11:28 +0700

    \

--- library/src/main/java/com/qwert2603/base_mvp/base/list/ListViewState.kt
+++ library/src/main/java/com/qwert2603/base_mvp/base/list/ListViewState.kt
@@ -1,7 +1,6 @@
 package com.qwert2603.base_mvp.base.list
 
 import com.qwert2603.base_mvp.base.BaseStatePartialChanges
-import com.qwert2603.base_mvp.base.BaseViewState
 import com.qwert2603.base_mvp.base.BaseViewStateContainer
 
 enum class ListState {
@@ -11,11 +10,10 @@ enum class ListState {
 }
 
 data class ListViewState<out T>(
-        override val baseViewState: BaseViewState,
         val listState: ListState,
         val list: List<T>,
         val scrollToTop: Boolean
-) : BaseViewStateContainer
+)
 
 interface ListViewStateContainer<out T> : BaseViewStateContainer {
     val listViewState: ListViewState<T>
