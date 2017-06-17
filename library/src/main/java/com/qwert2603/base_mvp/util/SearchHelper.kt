533b52e673b5d5a9e1a2841fc556e014dcc58c00
alex <qwert2603@mail.ru>
2017-06-17 14:38:32 +0700

    \

--- library/src/main/java/com/qwert2603/base_mvp/util/SearchHelper.kt
+++ library/src/main/java/com/qwert2603/base_mvp/util/SearchHelper.kt
@@ -32,10 +32,8 @@ class SearchHelper<T>(
 
     fun Single<List<T>>.filterResults(q: String) = filterList { nameResult(it).toLowerCase().contains(q) }
     fun Single<List<T>>.sortResults(query: String): Single<List<T>> = map { it.sortSearchResults(nameResult, query) }
-
-    companion object {
-        fun <T> List<T>.sortSearchResults(toString: (T) -> String, search: String) =
-                if (search.isBlank()) sortedBy(toString)
-                else sortedBy(toString).sortedBy { toString(it).toLowerCase().indexOf(search.toLowerCase()).let { if (it >= 0) it else Int.MAX_VALUE } }
-    }
 }
+
+fun <T> List<T>.sortSearchResults(toString: (T) -> String, search: String) =
+        if (search.isBlank()) sortedBy(toString)
+        else sortedBy(toString).sortedBy { toString(it).toLowerCase().indexOf(search.toLowerCase()).let { if (it >= 0) it else Int.MAX_VALUE } }
