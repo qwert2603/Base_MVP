074f392b8070451aa3959395f98d318e8664136b
alex <qwert2603@mail.ru>
2017-07-04 19:43:13 +0700

    \

--- library/src/main/java/com/qwert2603/base_mvp/navigation/BaseMainActivity.kt
+++ library/src/main/java/com/qwert2603/base_mvp/navigation/BaseMainActivity.kt
@@ -273,7 +273,10 @@ abstract class BaseMainActivity : AppCompatActivity(), Navigation {
     override fun isInBackStack(backStackItem: BackStackItem) = backStackItem.tag in backStack.map { it.tag }
 
     override fun showDialog(dialog: DialogFragment, tag: String) {
-        blockUI(80, { dialog.show(supportFragmentManager, tag) })
+        blockUI(80, {
+            blockUI(1000)
+            dialog.show(supportFragmentManager, tag)
+        })
     }
 
     override fun onBackPressed() {
